a695f88f1ae9af1cdc1201539e3efbf5b97a3f55
tran chinh <chinh99078@donga.edu.vn>
2023-09-18 21:01:41 +0700

    Sửa lỗi và hoàn thiện phần Header #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
+import routesConfig from '~/config/routes';
 //import layout
 import { HeaderOnly } from '~/components/Layout';
 
@@ -10,11 +11,11 @@ import Search from '~/pages/Search';
 
 // Dùng cho các routes không đăng nhập mà vẫn xem được
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/:nickname', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
-    { path: '/search', component: Search, layout: null },
+    { path: routesConfig.home, component: Home },
+    { path: routesConfig.following, component: Following },
+    { path: routesConfig.profile, component: Profile },
+    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
+    { path: routesConfig.search, component: Search, layout: null },
 ];
 
 // Dùng cho các routes phải đăng nhập mới xem được. Còn nếu không đăng nhập thì sẽ chuyển sang trang Login
