cdaba7fda4d07c5b58f8fac846cea5bcdceb0cfa
tran chinh <chinh99078@donga.edu.vn>
2023-09-19 00:14:28 +0700

    Tái cấu trúc và tối ưu code #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
-import routesConfig from '~/config/routes';
+import config from '~/config';
+
 //import layout
-import { HeaderOnly } from '~/components/Layout';
+import { HeaderOnly } from '~/layouts';
 
 //Pages
 import Home from '~/pages/Home';
@@ -11,11 +12,11 @@ import Search from '~/pages/Search';
 
 // Dùng cho các routes không đăng nhập mà vẫn xem được
 const publicRoutes = [
-    { path: routesConfig.home, component: Home },
-    { path: routesConfig.following, component: Following },
-    { path: routesConfig.profile, component: Profile },
-    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
-    { path: routesConfig.search, component: Search, layout: null },
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
 ];
 
 // Dùng cho các routes phải đăng nhập mới xem được. Còn nếu không đăng nhập thì sẽ chuyển sang trang Login
